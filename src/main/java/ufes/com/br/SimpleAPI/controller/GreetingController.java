823ba8222432b1b9031800b44d5b434bd315faae
Thiago Valentim <thiago_valentim1@hotmail.com>
2020-11-23 18:35:17 -0300

    Adição da resposta do Home

--- src/main/java/ufes/com/br/SimpleAPI/controller/GreetingController.java
+++ src/main/java/ufes/com/br/SimpleAPI/controller/GreetingController.java
@@ -16,4 +16,10 @@ public class GreetingController {
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
+
+    @GetMapping("/")
+    public Greeting home() {
+        return new Greeting(0, "Servidor Inicializado!");
+    }
 }
+
