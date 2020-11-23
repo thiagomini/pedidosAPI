427f32a5513bfbb2ddf724217c532185ae98cb45
Miqueas Amorim <miqueas.at@gmail.com>
2020-11-23 15:53:21 -0300

    Casos de testes de PedidoController

---
+++ src/test/java/ufes/com/br/SimpleAPI/controller/PedidoControllerTest.java
@@ -0,0 +1,60 @@
+package ufes.com.br.SimpleAPI.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ufes.com.br.SimpleAPI.factory.ProdutoFactory;
+import ufes.com.br.SimpleAPI.model.Produto;
+
+@WebMvcTest
+public class PedidoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @BeforeEach
+    public void setup() {
+        JacksonTester.initFields(this, new ObjectMapper());
+    }
+
+    @DisplayName("CT01 - Deve retornar uma lista vazia")
+    @Test
+    void CT01() throws Exception {
+        this.mockMvc.perform(get("/pedidos")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("[]"));
+    }
+
+    @DisplayName("CT02 - Deve retornar erro NOT FOUND")
+    @Test
+    void CT02() throws Exception {
+        this.mockMvc.perform(get("/pedidos/1"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @DisplayName("CT03 - Deve cadastrar um pedido corretamente")
+    @Test
+    void CT03() throws Exception {
+        Produto novoProduto = ProdutoFactory.createProduto("Leite","LT", 3.49, 10, "Leite");
+        this.mockMvc.perform(post("/pedidos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("[{\"idProduto\": 1, \"quantidade\": 5}]"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
